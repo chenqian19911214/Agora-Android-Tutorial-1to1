fdd2d72ee923b2956bb0b3c4f913c93649efc6b4
chenqian <1096011884@qq.com>
2018-03-31 23:49:16 +0800

    Initial commit

---
+++ app/src/main/java/io/agora/tutorials1v1vcall/VideoChatViewActivity.java
@@ -0,0 +1,307 @@
+package io.agora.tutorials1v1vcall;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.PorterDuff;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import io.agora.rtc.Constants;
+import io.agora.rtc.IRtcEngineEventHandler;
+import io.agora.rtc.RtcEngine;
+import io.agora.rtc.video.VideoCanvas;
+
+public class VideoChatViewActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = VideoChatViewActivity.class.getSimpleName();
+
+    private static final int PERMISSION_REQ_ID_RECORD_AUDIO = 22;
+    private static final int PERMISSION_REQ_ID_CAMERA = PERMISSION_REQ_ID_RECORD_AUDIO + 1;
+
+    private RtcEngine mRtcEngine;// Tutorial Step 1
+    private final IRtcEngineEventHandler mRtcEventHandler = new IRtcEngineEventHandler() { // Tutorial Step 1
+
+        //远端视频接收解码回调
+        @Override
+        public void onFirstRemoteVideoDecoded(final int uid, int width, int height, int elapsed) { // Tutorial Step 5
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    setupRemoteVideo(uid);
+                }
+            });
+        }
+
+        //加入频道回调
+        @Override
+        public void onJoinChannelSuccess(String channel, int uid, int elapsed) {
+            super.onJoinChannelSuccess(channel, uid, elapsed);
+        }
+
+
+        //其他用户离开当前频道回调
+        @Override
+        public void onUserOffline(int uid, int reason) { // Tutorial Step 7
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    onRemoteUserLeft();
+                }
+            });
+        }
+
+        //其他用户已停发/已重发视频流回调
+        @Override
+        public void onUserMuteVideo(final int uid, final boolean muted) { // Tutorial Step 10
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    onRemoteUserVideoMuted(uid, muted);
+                }
+            });
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_chat_view);
+
+        if (checkSelfPermission(Manifest.permission.RECORD_AUDIO, PERMISSION_REQ_ID_RECORD_AUDIO) && checkSelfPermission(Manifest.permission.CAMERA, PERMISSION_REQ_ID_CAMERA)) {
+            initAgoraEngineAndJoinChannel();
+        }
+    }
+
+    private void initAgoraEngineAndJoinChannel() {
+        initializeAgoraEngine();     // 初始化 视频通话
+        setupVideoProfile();         // 打开视频模式
+        setupLocalVideo();           // 创建本地视频预览
+        joinChannel();               // 建立连接
+    }
+
+    public boolean checkSelfPermission(String permission, int requestCode) {
+        Log.i(LOG_TAG, "checkSelfPermission " + permission + " " + requestCode);
+        if (ContextCompat.checkSelfPermission(this,
+                permission)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{permission},
+                    requestCode);
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String permissions[], @NonNull int[] grantResults) {
+        Log.i(LOG_TAG, "onRequestPermissionsResult " + grantResults[0] + " " + requestCode);
+
+        switch (requestCode) {
+            case PERMISSION_REQ_ID_RECORD_AUDIO: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    checkSelfPermission(Manifest.permission.CAMERA, PERMISSION_REQ_ID_CAMERA);
+                } else {
+                    showLongToast("No permission for " + Manifest.permission.RECORD_AUDIO);
+                    finish();
+                }
+                break;
+            }
+            case PERMISSION_REQ_ID_CAMERA: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    initAgoraEngineAndJoinChannel();
+                } else {
+                    showLongToast("No permission for " + Manifest.permission.CAMERA);
+                    finish();
+                }
+                break;
+            }
+        }
+    }
+
+    public final void showLongToast(final String msg) {
+        this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        leaveChannel();
+        RtcEngine.destroy();
+        mRtcEngine = null;
+    }
+
+    /**
+     * 暂定本地视频
+     * */
+    public void onLocalVideoMuteClicked(View view) {
+        ImageView iv = (ImageView) view;
+        if (iv.isSelected()) {
+            iv.setSelected(false);
+            iv.clearColorFilter();
+        } else {
+            iv.setSelected(true);
+            iv.setColorFilter(getResources().getColor(R.color.colorPrimary), PorterDuff.Mode.MULTIPLY);
+        }
+
+        mRtcEngine.muteLocalVideoStream(iv.isSelected());
+
+        FrameLayout container = (FrameLayout) findViewById(R.id.local_video_view_container);
+        SurfaceView surfaceView = (SurfaceView) container.getChildAt(0);
+        surfaceView.setZOrderMediaOverlay(!iv.isSelected());
+        surfaceView.setVisibility(iv.isSelected() ? View.GONE : View.VISIBLE);
+    }
+
+    /**
+     * 静音操作
+     * */
+    public void onLocalAudioMuteClicked(View view) {
+        ImageView iv = (ImageView) view;
+        if (iv.isSelected()) {
+            iv.setSelected(false);
+            iv.clearColorFilter();
+        } else {
+            iv.setSelected(true);
+            iv.setColorFilter(getResources().getColor(R.color.colorPrimary), PorterDuff.Mode.MULTIPLY);
+        }
+
+        mRtcEngine.muteLocalAudioStream(iv.isSelected());
+    }
+
+    /**
+     * 切换摄像头
+     * */
+    public void onSwitchCameraClicked(View view) {
+        mRtcEngine.switchCamera();
+    }
+
+    /**
+     * 关闭
+     * */
+    public void onEncCallClicked(View view) {
+        finish();
+    }
+
+    /**
+     * 初始化 视频通话
+     * */
+    private void initializeAgoraEngine() {
+        try {
+            mRtcEngine = RtcEngine.create(getBaseContext(), getString(R.string.agora_app_id), mRtcEventHandler);
+
+        } catch (Exception e) {
+            Log.e(LOG_TAG, Log.getStackTraceString(e));
+
+            throw new RuntimeException("NEED TO check rtc sdk init fatal error\n" + Log.getStackTraceString(e));
+        }
+    }
+
+    /**
+     * 设置本地视频 属性
+     * */
+    private void setupVideoProfile() {
+        mRtcEngine.enableVideo();
+        mRtcEngine.setVideoProfile(Constants.VIDEO_PROFILE_360P, false);//设置本地视频属性
+    }
+
+    /**
+     * 创建渲染图层
+     * */
+    private void setupLocalVideo() {
+        FrameLayout container = (FrameLayout) findViewById(R.id.local_video_view_container);
+        SurfaceView surfaceView = RtcEngine.CreateRendererView(getBaseContext()); //创建渲染视图
+        surfaceView.setZOrderMediaOverlay(true);
+        container.addView(surfaceView);
+        mRtcEngine.setupLocalVideo(new VideoCanvas(surfaceView, VideoCanvas.RENDER_MODE_ADAPTIVE, 0));//设置本地视频显示属性
+    }
+
+    /**
+     * 加入频道
+     * */
+    private void joinChannel() {
+
+        /**
+         * s  token
+         *
+         * s1 channelName 标识通话的频道名称
+         *
+         * s2 optionalInfo 开发者需加入的任何附加信息。一般可设置为空字符串，或频道相关信息
+         *
+         * i optionalUid  用户ID，32位无符号整数。建议设置范围：1到(2^32-1)，并保证唯一性。如果不指定（即设为0），SDK 会自动分配一个，并在 onJoinChannelSuccess 回调方法中返回，App 层必须记住该返回值并维护，SDK不对该返回值进行维护
+            uid 在 SDK 内部用 32 位无符号整数表示，由于 Java 不支持无符号整数，uid 被当成 32 位有符号整数处理，对于过大的整数，Java 会表示为负数，如有需要可以用(uid&0xffffffffL)转换成 64 位整数
+         * */
+        mRtcEngine.joinChannel(null, "demoChannel1", "Extra Optional Data", 0); // if you do not specify the uid, we will generate the uid for you
+    }
+
+    /**
+     * 显示 对方信息
+     * */
+    private void setupRemoteVideo(int uid) {
+        FrameLayout container = (FrameLayout) findViewById(R.id.remote_video_view_container);
+
+        if (container.getChildCount() >= 1) {
+            return;
+        }
+
+        SurfaceView surfaceView = RtcEngine.CreateRendererView(getBaseContext());
+        container.addView(surfaceView);
+        mRtcEngine.setupRemoteVideo(new VideoCanvas(surfaceView, VideoCanvas.RENDER_MODE_ADAPTIVE, uid));
+
+        surfaceView.setTag(uid); // for mark purpose
+        View tipMsg = findViewById(R.id.quick_tips_when_use_agora_sdk); // optional UI
+        tipMsg.setVisibility(View.GONE);
+    }
+
+    /**
+     * 离开频道
+     * */
+    private void leaveChannel() {
+        mRtcEngine.leaveChannel();
+    }
+
+    /**
+     * 用户离开 销毁显示对方视图
+     * */
+    private void onRemoteUserLeft() {
+        FrameLayout container = (FrameLayout) findViewById(R.id.remote_video_view_container);
+        container.removeAllViews();
+
+        View tipMsg = findViewById(R.id.quick_tips_when_use_agora_sdk); // optional UI
+        tipMsg.setVisibility(View.VISIBLE);
+    }
+
+    /**
+     * 其他用户已停发/已重发视频流回调
+     *
+     * @param uid 用户uid
+     * @param muted  true 回复  false  停发
+     * */
+    private void onRemoteUserVideoMuted(int uid, boolean muted) {
+        FrameLayout container = (FrameLayout) findViewById(R.id.remote_video_view_container);
+
+        SurfaceView surfaceView = (SurfaceView) container.getChildAt(0);
+
+        Object tag = surfaceView.getTag();
+        if (tag != null && (Integer) tag == uid) {
+            surfaceView.setVisibility(muted ? View.GONE : View.VISIBLE);
+        }
+    }
+}
